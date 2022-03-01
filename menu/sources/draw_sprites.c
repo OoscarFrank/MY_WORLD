72c3f6dcd2d0ab904f70cddba916f3e39919cf8c
Oscar FRANK <oscar.frank@epitech.eu>
2022-03-01 11:04:46 +0100

    menu

---
+++ menu/sources/draw_sprites.c
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2021
+** ouais
+** File description:
+** draw_sprites.c
+*/
+
+#include "../includes/world.h"
+
+void draw_spwelcome(window *wndw, options *sprt)
+{
+    sfRenderWindow_drawSprite(wndw->window, sprt->spwelcome, NULL);
+}
+
+void draw_spfond(window *wndw, options *sprt, int i)
+{
+    sfVector2f prems = {40, 65 - 915 + i};
+    sfSprite_setPosition(sprt->spfond, prems);
+    sfRenderWindow_drawSprite(wndw->window, sprt->spfond, NULL);
+}
+
+void draw_spbarre(window *wndw, options *sprt)
+{
+    sfRenderWindow_drawSprite(wndw->window, sprt->spbarre, NULL);
+}
+
+void draw_spmenu(window *wndw, options *sprt)
+{
+    sfRenderWindow_drawSprite(wndw->window, sprt->spmenu, NULL);
+}
+
+void draw_spcuts(window *wndw, options *sprt)
+{
+    sfRenderWindow_drawSprite(wndw->window, sprt->spcuts, NULL);
+}
---
+++ menu/sources/main.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2021
+** ouais
+** File description:
+** main.c
+*/
+
+#include "../includes/world.h"
+
+int call_main_func(char *str)
+{
+    window *wndw = malloc(sizeof(window));
+    options *sprt = malloc(sizeof(options));
+    main_func(wndw, sprt);
+}
+
+int main(int ac, char **av)
+{
+    call_main_func(av[1]);
+}
